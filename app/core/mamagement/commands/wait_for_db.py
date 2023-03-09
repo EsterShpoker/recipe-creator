2414890afd6dbf461cbef6baef409c542b3aa173
EsterShpoker <59788650+EsterShpoker@users.noreply.github.com>
2023-03-09 15:48:44 +0200

    wait_for_db test

---
+++ app/core/mamagement/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django cpmmand to wait for the database to be available.
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django cpmmand to wait for the database to be available."""
+
+    def handle(self, *args, **options):
+        pass
