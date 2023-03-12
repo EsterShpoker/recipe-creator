1792d89a96ff0e5d7dbbce8647c5e7d70469341e
EsterShpoker <59788650+EsterShpoker@users.noreply.github.com>
2023-03-12 12:08:18 +0200

    email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,7 @@ class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful."""
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -25,14 +25,14 @@ class ModelTests(TestCase):
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST@example.com'],
             ['test4@example.COM', 'test4@eample.com']
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects/create_user(email, 'ssample123')
+            user = get_user_model().objects.create_user(email, 'ssample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """Test creating a new user without an email raises ValueError."""
+        """Test that creating a user without an email raises ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(' ', 'test123')
+            get_user_model().objects.create_user('', 'test123')
