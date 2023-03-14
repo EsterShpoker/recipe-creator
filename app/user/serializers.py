b8562d04f5b159a7eaf012228d929b11d9f9711f
EsterShpoker <59788650+EsterShpoker@users.noreply.github.com>
2023-03-14 15:34:35 +0200

    test authentication

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -25,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
     email = serializers.EmailField()
-    password = serializers.CharFiels(
+    password = serializers.CharFiles(
         style={'input_type': 'password'},
         trim_whitespace=False,
     )
