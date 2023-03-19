e1c4f95e8e74c7844f14c49e912962ce9b24e435
EsterShpoker <59788650+EsterShpoker@users.noreply.github.com>
2023-03-20 00:47:23 +0200

    create recipe added

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -13,10 +13,11 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
-app_name ='recipe'
+app_name = 'recipe'
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/recipe/', include('recipe.urls')),
 ]
-
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,10 @@
 """
-Views for the recipe APIs.
+Views for the recipe APIs
 """
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -14,7 +17,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -24,8 +27,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
-        """Return appropriate serializer class."""
-        if self.action =='list':
+        """Return the serializer class for request."""
+        if self.action == 'list':
             return serializers.RecipeSerializer
 
         return self.serializer_class
@@ -35,7 +38,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(viewsets.ModelViewSet):
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -43,6 +46,6 @@ class TagViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """Retrieve tags for authenticated user."""
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
